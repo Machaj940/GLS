7877672407c626ed2376ca03478aafb66139967b
John Macharia <machariajohn23@gmail.com>
2023-04-20 21:33:33 +0300

    Test that Client is picked up by File Storage

---
+++ test_save_reload_user.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from models import storage
+from models.base_model import BaseModel
+from models.client import Client
+
+all_objs = storage.all()
+print("-- Reloaded objects --")
+for obj_id in all_objs.keys():
+    obj = all_objs[obj_id]
+    print(obj)
+
+print("-- Create a new User --")
+my_client = Client()
+my_client.full_name = "Betty"
+my_client.email = "airbnb@mail.com"
+my_client.password = "root"
+my_client.save()
+print(my_client)
+
+print("-- Create a new User 2 --")
+my_client2 = Client()
+my_client2.full_name = "John"
+my_client2.email = "airbnb2@mail.com"
+my_client2.password = "root"
+my_client2.save()
+print(my_client2)
