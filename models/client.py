eda027e94f6e4788cd0f00b87610724e7a8f107b
John Macharia <machariajohn23@gmail.com>
2023-04-20 19:12:54 +0300

    Client class represents the clients that subscribe to GLS

---
+++ models/client.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Class client represents the clients that subscribe to GLS"""
+
+
+from models.base_model import BaseModel
+
+
+class Client(BaseModel):
+    """the User class"""
+    full_name = ""
+    Business_name = ""
+    email = ""
+    password = ""
+    location = ""
